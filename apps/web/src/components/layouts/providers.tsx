46f4de294b691e67eb2757c4d0fcc48372ad90d7
Yash Gourav Kar <yashgouravkar@gmail.com>
2025-11-12 11:12:29 +0530

    Initial commit

---
+++ apps/web/src/components/layouts/providers.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'next-themes';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import { Toaster } from 'sonner';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Toaster richColors closeButton theme="system" />
+      <NuqsAdapter>{children}</NuqsAdapter>
+    </ThemeProvider>
+  );
+}
