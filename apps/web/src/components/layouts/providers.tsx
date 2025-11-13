fa8f8df8b7f2823823d9bb72f34367d35788cd93
yash <yashgouravkar@gmail.com>
2025-11-13 18:48:20 +0530

    ss

--- apps/web/src/components/layouts/providers.tsx
+++ apps/web/src/components/layouts/providers.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'sonner';
@@ -7,7 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="light"
+      defaultTheme="system"
       enableSystem
       disableTransitionOnChange
     >
